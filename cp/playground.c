45addf8e257f45b74ac32a2d339fb980810939fc
LuciusPlamondon <luciusplamondon2006@gmail.com>
2026-01-20 22:16:38 -0700

    poggers

---
+++ cp/playground.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  int x =10;
+  float z = 25.537;
+  char c = '^';
+  printf("%d\n%f\n%c\n", x, z, c);
+  printf("Two significant digits z: %.2f\n", z);
+}
