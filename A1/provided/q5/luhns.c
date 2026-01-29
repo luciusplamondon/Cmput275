c1867e0180695c37681d1aeda2af527a54ca2ace
LuciusPlamondon <luciusplamondon2006@gmail.com>
2026-01-28 21:14:52 -0700

    q5

--- A1/provided/q5/luhns.c
+++ A1/provided/q5/luhns.c
@@ -1,2 +1,30 @@
 #include <stdio.h>
-
+int main() {
+int x;
+int y;
+int z;
+int sum;
+sum = 0;
+scanf("%d",&x);
+y = x%10;
+x = x/10;
+z = y;
+while (x > 0) {
+    y = x%10;
+    x = x/10;
+    if (y*2 >= 10) {
+        sum = sum + (2*y - 9);
+    } else {
+        sum = sum + 2*y;
+    }
+    y = x%10;
+    x = x/10;
+    sum = sum + y;
+    }
+if ((sum*9)%10 == z) {
+    printf("Valid\n");
+} 
+else {
+    printf("Invalid\n");
+}
+}
