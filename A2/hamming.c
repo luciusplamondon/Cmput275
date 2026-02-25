6129180ec58ef79278f0616341e6a57145f1e837
LuciusPlamondon <luciusplamondon2006@gmail.com>
2026-02-24 17:04:56 -0700

    fixed q1 and q2

--- A2/hamming.c
+++ A2/hamming.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int hammingbase(int num1, int num2, int base){
-    int hamming = 0;
+unsigned int hammingbase(unsigned int num1, unsigned int num2, unsigned int base){
+    unsigned int hamming = 0;
     while (num1 != 0 || num2 != 0){
     if ((num1%base) != (num2%base)){
         ++hamming;
@@ -16,9 +16,9 @@ int main(int argc, char **argv) {
         printf("Usage: ./hamming n1 n2 base\n");
     }
     else{
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    int base = atoi(argv[3]);
+    unsigned int num1 = atoi(argv[1]);
+    unsigned int num2 = atoi(argv[2]);
+    unsigned int base = atoi(argv[3]);
     printf("%d\n",hammingbase(num1,num2,base));
 }
 }
--- A2/sstring.c
+++ A2/sstring.c
@@ -10,10 +10,22 @@ char* increaseCap(char *s, unsigned int *capacity) {  //function that doubles th
     *capacity = newCap; //update the capacity indicator from caps[]
     return newArr;
 }
+void print(char *s, unsigned int *size) { // simple print function
+    for (int i = 0; i < *size; i++ ) {
+        printf("%c",s[i]);
+    }
+}
+void display(char *s, unsigned int *size, unsigned int *capacity){
+    printf("String: \"");
+    print(s, size);
+    printf("\"\nLength: %d",*size);
+    printf("\nCapacity: %d\n",*capacity);
+
+}
 void read(char **s, unsigned int *size, unsigned int *capacity){
     *size = 0; //inputing completly new string so reset the size
     char x; //garbage variable, to grab input
-    while ((x = getchar()) != EOF && (x == ' ' || x == '\n' || x == '\t'));  //make sure we get ride of space char between var and the string
+    while ((x = getchar()) != EOF && (x == ' ' || x == '\n' || x == '\t' || x == '\r' || x == '\v' || x == '\f'));  //make sure we get ride of space char between var and the string
     int quote = 0;
     if (x == '"'){
         quote = 1;  //signaling quote mode
@@ -25,22 +37,21 @@ void read(char **s, unsigned int *size, unsigned int *capacity){
                 break;
             }}
         else{
-            if (x == ' ' || x == '\n' || x == '\t' ) { //if space char, stop loop
+            if (x == ' ' || x == '\n' || x == '\t' || x == '\r' || x == '\v' || x == '\f') { //if space char, stop loop
                 break;
             }
         }
-        if (*size == *capacity-1) {    // if arrat full, double capacity
+        if (*size == *capacity) {    // if arrat full, double capacity
             *s = increaseCap(*s, capacity);
             }
         (*s)[(*size)++] = x;  //put nth char in nth location in array
         x = getchar();   //get next char
         }
-        (*s)[*size] = '\0'; //end of string
 }
 void append(char **s, unsigned int *size, unsigned int *capacity){ // code is the same except we arent resetting the size
     // notice we did not reset the size, simply increment size by one will be we appending values into *s
     char x; //garbage variable, to grab input
-    while ((x = getchar()) != EOF && (x == ' ' || x == '\n' || x == '\t'));  //make sure we get ride of space char between var and the string
+    while ((x = getchar()) != EOF && (x == ' ' || x == '\n' || x == '\t' || x == '\r' || x == '\v' || x == '\f'));  //make sure we get ride of space char between var and the string
     int quote = 0;
     if (x == '"'){
         quote = 1;  //signaling quote mode
@@ -52,22 +63,20 @@ void append(char **s, unsigned int *size, unsigned int *capacity){ // code is th
                 break;
             }}
         else{
-            if (x == ' ' || x == '\n' || x == '\t' ) { //if space char, stop loop
+            if (x == ' ' || x == '\n' || x == '\t' || x == '\r' || x == '\v' || x == '\f') { //if space char, stop loop
                 break;
             }
         }
-        if (*size == *capacity-1) {    // if arrat full, double capacity
+        if (*size == *capacity) {    // if arrat full, double capacity
             *s = increaseCap(*s, capacity);
             }
         (*s)[(*size)++] = x;  //put nth char in nth location in array
         x = getchar();   //get next char
         }
-        (*s)[*size] = '\0'; //end of string
 }
 void concadenate(char **t1, unsigned int *size1, unsigned int *cap1, char *t2, unsigned int size2, char *t3, unsigned int size3){
     *size1 = 0; // resetting var
-    (*t1)[0] = '\0'; //this time we put null teminator because we are going to simply add using for loops
-    while (*cap1 <= size2 + size3 + 1) { //making sure our array size of t1 is big enough for the process
+    while (*cap1 < size2 + size3) { //making sure our array size of t1 is big enough for the process
         *t1 = increaseCap(*t1, cap1);
     }
     for (unsigned int i = 0; i < size2; i++) { //adding *t2 into *t1
@@ -76,7 +85,6 @@ void concadenate(char **t1, unsigned int *size1, unsigned int *cap1, char *t2, u
     for (unsigned int i = 0; i < size3; i++) {//adding *t3 into *t1
         (*t1)[(*size1)++] = t3[i];
     }
-    (*t1)[*size1] = '\0';//end of string
 }
 int main(int argc, char const *argv[]){
     char *a = malloc(4); //setting up array for the variables
@@ -96,25 +104,31 @@ int main(int argc, char const *argv[]){
         else if (x == 'r'){ // if r is receive, trigger the read step
             scanf(" %c",&x); //the space before %c in scanf indicate skiping whitespace before it, check what variable it will use
             if ( x >= 'a' && x <= 'd' ) read(&var[x-'a'],&sizes[x-'a'], &caps[x-'a']); // check if variable is a-d, since we have those variable in the array var, we can use x-'a' for the variable we need along with is recorded size stored in sizes and caps array
-            else printf("Invalid SString target %c\n", x);// if not a-d, tell the use its invalid
+            else{printf("Invalid SString target %c\n", x); break;}// if not a-d, tell the use its invalid
         }
         else if (x == 'p'){ // if p is receive, trigger the print step
             scanf(" %c",&x);//like in read, check what variable we will print
-            if ( x >= 'a' && x <= 'd' ) printf("%s\n", var[x-'a']);// like in read, we check if variable is valid and simply print its contents
-            else printf("Invalid SString target %c\n", x);//if not a-d signal the user that its invalid
+            if ( x >= 'a' && x <= 'd' ) print(var[x-'a'],&sizes[x-'a']);// like in read, we check if variable is valid and simply print its contents
+            else {printf("Invalid SString target %c\n", x); break;}//if not a-d signal the user that its invalid
+            printf("\n");
+        }
+        else if (x == 'd'){
+            scanf(" %c",&x); //the space before %c in scanf indicate skiping whitespace before it, check what variable it will use
+            if ( x >= 'a' && x <= 'd' ) display(var[x-'a'],&sizes[x-'a'], &caps[x-'a']); 
+            else {printf("Invalid SString target %c\n", x); break;}// if not a-d, tell the use its invalid
         }
         else if (x == 'a') {// if a is receive, trigger the append step
            scanf(" %c",&x);
             if ( x >= 'a' && x <= 'd' ) append(&var[x-'a'],&sizes[x-'a'], &caps[x-'a']);
-           else printf("Invalid SString target %c\n", x);//if not a-d signal the user that its invalid
+           else {printf("Invalid SString target %c\n", x); break;}//if not a-d signal the user that its invalid
         }
         else if (x == 'c') { // concadenate step
             scanf(" %c", &x); // we need to check three variables now
             scanf(" %c", &y);
             scanf(" %c", &z);
-            if ( x < 'a' && x > 'd' ) printf("Invalid SString target %c\n", x); // checking if var in x, y, z are valid
-            else if ( y < 'a' && y > 'd' ) printf("Invalid SString target %c\n", y);
-            else if ( z < 'a' && z > 'd' ) printf("Invalid SString target %c\n", z);
+            if ( x < 'a' && x > 'd' ) {printf("Invalid SString target %c\n", x); break;} // checking if var in x, y, z are valid
+            else if ( y < 'a' && y > 'd' ) {printf("Invalid SString target %c\n", y); break;}
+            else if ( z < 'a' && z > 'd' ) {printf("Invalid SString target %c\n", z); break;}
             else concadenate(&var[x-'a'], &sizes[x-'a'], &caps[x-'a'], var[y-'a'], sizes[y-'a'], var[z-'a'], sizes[z-'a']);//little to many variable imo lmao
         }
     
